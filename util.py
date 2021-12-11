cf458e94917578116c36b67aab316699197b80fe
Jasper Snel <me@jaspersnel.me>
2021-12-11 20:08:39 +0100

    remove arbitrary kwargs

--- util.py
+++ util.py
@@ -2,10 +2,13 @@ from abc import ABC, abstractmethod
 from pathlib import Path
 
 class AbstractSource(ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
     @abstractmethod
-    def import_file(self, path: Path, **kwargs):
+    def import_file(self, path: Path):
         pass
 
     @abstractmethod
-    def export_file(self, path: Path, **kwargs):
+    def export_file(self, path: Path):
         pass
