c3b86e4411b62d4098f5babe8b83ada7b92c79fa
Jasper Snel <me@jaspersnel.me>
2021-12-13 19:46:25 +0100

    Add comma

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     packages=['datasources'],
     python_requires='>=3.6',
     install_requires=[
-        'rdflib'
+        'rdflib',
         'SPARQLWrapper',
         'requests'
     ]
